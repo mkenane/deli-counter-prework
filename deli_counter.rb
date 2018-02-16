fa72d9ee6458e7a9cc4f7a7cd67d691b80bcdebc
Mor Kenane <morkenan@hotmail.com>
2018-02-16 20:53:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,17 @@
+
 def line (array)
-  array.each_with_index
   
+  linearray = []
+  
+if array.length == 0 
+  return "The line is currently empty."
+  
+else 
+  array.each_with_index.map {|name, i|  
+  linearray.push("#{i+1}. #{name}")}
+
+  linestring = linearray.join(" ")
+  "The line is currently: #{linestring}"
+  end 
+
+end 
